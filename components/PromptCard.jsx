f2f5f461de35bc6f7dfe82278ee8a9fe21655edd
HeinHtet14120 <heinhtet14120@gmail.com>
2024-08-12 00:04:09 +0700

    editdelete

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -15,7 +15,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setCopied(""), 3000)
+    setTimeout(() => setCopied(""), 1000)
   }
   return (
     <div className=" prompt_card">
@@ -43,7 +43,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <p className=" font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
 
       {session?.user.id === post.creator._id && pathName === '/profile' && (

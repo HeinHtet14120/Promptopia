2e240816b685c899f28b160f0b27b79ccf90a5f1
HeinHtet14120 <heinhtet14120@gmail.com>
2024-08-12 21:13:10 +0700

    search

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -21,10 +21,8 @@ const Feed = () => {
 
   const [searchText, setSearchText] = useState('')
   const [posts, setPosts] = useState([]);
+  const [searchPosts, setSearchPosts] = useState([])
 
-  const handleSearchChange = async (e) => {
-
-  }
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -33,9 +31,40 @@ const Feed = () => {
       setPosts(data);
     }
 
-    console.log(posts)
     fetchPosts();
   }, [])
+
+
+  const searchFun = (searchText) => {
+
+    const regex = new RegExp(searchText, "i"); // 'i' flag for case-insensitive search
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+
+  }
+
+  const handleSearchChange = (e) => {
+    const searchQuery = e.target.value.toLowerCase();
+    setSearchText(searchQuery);
+
+    if (searchText) {
+
+      const searchResults = searchFun(searchText);
+
+      setSearchPosts(searchResults);
+    }
+  }
+
+  const handleTagClick = (tagText) => {
+    setSearchText(tagText);
+    const restult = searchFun(tagText);
+    setSearchPosts(restult)
+  }
+
   return (
     <section className="feed">
       <form className=" relative w-full flex-center">
@@ -47,10 +76,19 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList
-        data={posts}
-        handleTagClick={() => { }}
-      />
+
+      {
+        searchText ?
+          <PromptCardList
+            data={searchPosts}
+            handleTagClick={handleTagClick}
+          /> :
+          <PromptCardList
+            data={posts}
+            handleTagClick={handleTagClick}
+          />
+      }
+
     </section>
   )
 }
